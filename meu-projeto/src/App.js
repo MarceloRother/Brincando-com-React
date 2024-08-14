f9dc988200bb53ee492cfd6d42f761c1ebba1044
MarceloRother <marcelorotherwork@gmail.com>
2024-08-14 12:43:47 +0100

    Quarta aula finalizada - React Fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -17,7 +18,9 @@ function App() {
         nome="Marcelo" 
         idade="20" 
         profissao="Estudante" 
-        foto="https://via.placeholder.com/150"/>
+        foto="https://via.placeholder.com/150"
+      />
+      <List/>
     </div>
   );
 }
