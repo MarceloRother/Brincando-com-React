e7229c5e20b525e0e31b613abb322e625007e373
MarceloRother <marcelorotherwork@gmail.com>
2024-08-14 13:52:51 +0100

    Correcao PorpTypes

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 
-function Item({ marca, ano_lancamento }) {
+function Item({ marca=0, ano_lancamento='Faltu a marca' }) {
     return (
         <>
         <li>{marca} - {ano_lancamento}</li>
@@ -8,14 +8,7 @@ function Item({ marca, ano_lancamento }) {
     )
 }
 
-Item.prototype = {
-    marca: PropTypes.string.isRequired,
-    ano_lancamento: PropTypes.number
-}
-
-Item.defaultProps = {
-    marca: 'Faltou a marca',
-    ano_lancamento: 0
-}
-
 export default Item
+
+// Esse recurso (defaultProps e propType) está sendo depreciado para componentes funcionais e, em futuras versões principais do React, ele será removido.
+// Portante, passara a ser usado a sintaxe de parametros padrao para JS = { function Item({ marca=0, ano_lancamento='Faltu a marca' }) }
