0dd207e2c381c79e14d5b576b9d83aafc5b0f35b
MarceloRother <marcelorotherwork@gmail.com>
2024-08-14 15:41:44 +0100

    Aula sobre Eventos

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,26 +1,14 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Testando CSS</h1>
-      <Frase/>
-      <Frase/>
-      <SayMyName nome="Marcelo"/>
-      <SayMyName nome="Giulia"/>
-      <Pessoa 
-        nome="Marcelo" 
-        idade="20" 
-        profissao="Estudante" 
-        foto="https://via.placeholder.com/150"
-      />
-      <List/>
+      <Evento numero="1"/>
+      <Form/>
     </div>
   );
 }
