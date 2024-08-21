2cfe08b951ece6a73206965daba2035411ccac21
MarceloRother <marcelorotherwork@gmail.com>
2024-08-21 18:07:40 +0100

    React Icons

--- meu-projeto/src/components/layout/Footer.js
+++ meu-projeto/src/components/layout/Footer.js
@@ -1,7 +1,23 @@
+import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
+
+import styles from './Footer.module.css'
+
 function Footer() {
-    return(
-        <footer>Rodape</footer>
-    )
+  return (
+    <footer>
+      <ul className={styles['social_list']}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaGithub />
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
