30f2bf4de08b86f02e8d10c9e54336a525bbee08
MarceloRother <marcelorotherwork@gmail.com>
2024-09-03 11:52:13 +0100

    Criando Dashboard - Aula 9

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton";
+
+import styles from './Projects.module.css'
 
 function Projects() {
 
@@ -9,12 +13,16 @@ function Projects() {
   if(location.state) {
     message = location.state.message
   }
-
-
     return (
-      <div>
+      <div className={styles.project_container}>
+        <div className={styles.title_container}>
         <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto"/>
+        </div>
         {message && <Message type="success" msg={message} />}
+        <Container custimClass="start">
+          <p>Projetos</p>
+        </Container>
       </div>
     )
 }
